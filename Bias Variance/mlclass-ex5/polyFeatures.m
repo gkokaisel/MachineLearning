798b7b17b8f2f622fb607aa625ffa361e7dc0f3b
gkokaisel <gkokaisel@gmail.com>
2011-11-14 18:45:51 -0500

    Bias vs. Variance Complete
    Still trying to fully vectorize polyfeature function

---
+++ Bias Variance/mlclass-ex5/polyFeatures.m
@@ -0,0 +1,38 @@
+function [X_poly] = polyFeatures(X, p)
+%POLYFEATURES Maps X (1D vector) into the p-th power
+%   [X_poly] = POLYFEATURES(X, p) takes a data matrix X (size m x 1) and
+%   maps each example into its polynomial features where
+%   X_poly(i, :) = [X(i) X(i).^2 X(i).^3 ...  X(i).^p];
+%
+
+
+% You need to return the following variables correctly.
+X_poly = zeros(numel(X), p);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Given a vector X, return a matrix X_poly where the p-th 
+%               column of X contains the values of X to the p-th power.
+%
+% 
+
+% All three solutions work, but bsxfun produces a different figure. Also, bsxfun seems more accurate.
+% The Polynomial regression fit with bsxfun continues to climb instead of immediately drop off,
+% which is what the next two methods do. 
+m = size(X, 1);
+for i = 1:m
+p = (1:8);
+X_poly(i, :) = bsxfun(@power, X(i), p);
+end
+ 
+%for i = 1:p
+%X_poly(:, i) = X.^i;
+%end
+ 
+%for i = 1:p
+%X_poly(:, i) = power(X, i);
+%end
+
+
+% =========================================================================
+
+end
